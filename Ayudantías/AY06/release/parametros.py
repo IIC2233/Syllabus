9de7679f40ced0509d1e906ab214f9d053527b02
CLEMENTE CAMPOS <clemente.campos@uc.cl>
2025-09-23 12:46:56 -0300

    AY06

---
+++ Ayudantías/AY06/release/parametros.py
@@ -0,0 +1,8 @@
+APELLIDOS_SOSPECHOSOS = {
+    "Skywalker", "Solo", "Organa", "Andor", "Kenobi"
+}
+
+PLANETAS_REBELDES = {
+    "Yavin 4", "Dantooine", "Hoth", "Lothal", "Chandrila", "Mon Cala",
+    "Sullust", "Jedha", "Ryloth", "Alderaan", "Atollon", "Corellia"
+}
