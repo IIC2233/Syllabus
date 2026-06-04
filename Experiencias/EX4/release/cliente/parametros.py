7524874e0552cde0463e296457653edd233ea9e1
Dani Concha <daconcha@uc.cl>
2026-06-04 12:48:26 -0400

    😸 Clase semana 14

---
+++ Experiencias/EX4/release/cliente/parametros.py
@@ -0,0 +1,19 @@
+POSICION_Y = 300
+TIEMPO_CAIDA_METEORO = 0.1
+DISTANCIA_MINIMA = 5
+DISTANCIA_MAXIMA = 20
+
+DIFICULTAD = ["Fácil", "Normal", "Imposible"]
+
+FONDO_PATH = "./frontend/assets/fondo.jpg"
+METEORO_PATH = "./frontend/assets/meteorite.png"
+MIRA_PATH = "./frontend/assets/shooting-target.png"
+SOUND_PATH = "./frontend/files/ost.mp3"
+
+ANCHO_JUEGO = 800
+ALTURA_JUEGO = 500
+
+ANCHO_METEORO = 30
+ALTURA_METEORO = 200
+
+DISTANCIA_IMPACTO = 10
